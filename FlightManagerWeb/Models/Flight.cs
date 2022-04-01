8bcb3cc64df5355eb9053c011ec1e8e2717506ba
gani090803 <ganitemuras@gmail.com>
2022-04-01 16:36:43 +0300

    added models for Reservation and Flight

---
+++ FlightManagerWeb/Models/Flight.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManagerWeb.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string LocationFrom { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string LocationTo { get; set; }
+
+
+        public DateTime DepartureTimeAndDate { get; set; }
+
+
+        public DateTime ArrivalTimeAndDate { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string PlaneType { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string PlaneNumber { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string PilotName { get; set; }
+
+
+        [Range(0, 600)]
+        public int CapacityBusinessClass { get; set; }
+
+
+        [Range(0, 600)]
+        public int CapacityFirstClass { get; set; }
+
+
+        public List<Reservation>? reservations { get; set; }
+    }
+}
---
+++ FlightManagerWeb/Models/Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlightManagerWeb.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string SecondName { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string EGN { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string PhoneNumber { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string Nationality { get; set; }
+
+
+        [Required]
+        [MaxLength(30)]
+        public string TicketType { get; set; }
+
+
+        [ForeignKey("Flight")]
+        public int FlightId { get; set; }
+    }
+}
